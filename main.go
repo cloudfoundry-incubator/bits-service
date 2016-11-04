c9e85b63b22554a0f251c71907fe3202ea414c6f
Peter Goetz <peter.gtz@gmail.com>
2016-11-04 01:00:29 +0100

    Adding first attempts for package store

--- main.go
+++ main.go
@@ -11,12 +11,18 @@ import (
 
 func main() {
 	router := mux.NewRouter()
+	packageHandler := &PackageHandler{name: "TestServer"}
+	internalHostName := "ups.127.0.0.1.xip.io"
 
-	router.HandleFunc("/{name}", HomeHandler)
+	internalRouter := router.Host(internalHostName).Subrouter()
+
+	internalRouter.Path("/packages/{guid}").Methods("PUT").HandlerFunc(packageHandler.put)
+	internalRouter.Path("/packages/{guid}").Methods("GET").HandlerFunc(packageHandler.get)
+	internalRouter.Path("/packages/{guid}").Methods("DELETE").HandlerFunc(packageHandler.delete)
 
 	srv := &http.Server{
 		Handler:      router,
-		Addr:         "127.0.0.1:8000",
+		Addr:         "0.0.0.0:8000",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
@@ -24,8 +30,21 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-func HomeHandler(responseWriter http.ResponseWriter, request *http.Request) {
+type DataStore interface {
+}
 
-	fmt.Fprintf(responseWriter, "Hello, %s", mux.Vars(request)["name"])
+type PackageHandler struct {
+	name string
+}
 
+func (handler *PackageHandler) put(responseWriter http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(responseWriter, "%s says: Hello, %s\n\n", handler.name, mux.Vars(request)["guid"])
+}
+
+func (handler *PackageHandler) get(responseWriter http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(responseWriter, "%s says: Hello, %s\n\n", handler.name, mux.Vars(request)["guid"])
+}
+
+func (handler *PackageHandler) delete(responseWriter http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(responseWriter, "%s says: Hello, %s\n\n", handler.name, mux.Vars(request)["guid"])
 }
