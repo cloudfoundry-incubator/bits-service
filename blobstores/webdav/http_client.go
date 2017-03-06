fd3e4d16d6c0320c0f8d695fdc64f1d84d52f07c
Peter Goetz <peter.gtz@gmail.com>
2017-03-06 19:46:46 +0100

    Log details in NewHttpClient

--- blobstores/webdav/http_client.go
+++ blobstores/webdav/http_client.go
@@ -5,14 +5,22 @@ import (
 	"crypto/x509"
 	"net/http"
 	"time"
+
+	log "github.com/petergtz/bitsgo/logger"
+	"github.com/uber-go/zap"
 )
 
 func NewHttpClient(pemCerts string, insecureSkipVerify bool) *http.Client {
+	log.Log.Info("Creating Http Client",
+		zap.Bool("insecure-skip-verify", insecureSkipVerify),
+		zap.String("ca-cert-path", pemCerts))
+
 	caCertPool := x509.NewCertPool()
 	ok := caCertPool.AppendCertsFromPEM([]byte(pemCerts))
 	if !ok {
 		panic("Could not append pemCerts. pemCerts content:\n\n```\n" + pemCerts + "```")
 	}
+
 	return &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{
