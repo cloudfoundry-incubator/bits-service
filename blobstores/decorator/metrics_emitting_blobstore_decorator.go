5ec275dc9a0c08a715e86eca43cd77289ea98cf9
Peter Goetz <peter.gtz@gmail.com>
2018-04-16 10:12:24 +0200

    Add new metrics_emitting_blobstore_decorator
    This lets us refactor the ResourceHandler by removing ugly metrics statements from the business logic.

---
+++ blobstores/decorator/metrics_emitting_blobstore_decorator.go
@@ -0,0 +1,53 @@
+package decorator
+
+import (
+	"io"
+	"time"
+
+	"github.com/petergtz/bitsgo"
+)
+
+type MetricsEmittingBlobstoreDecorator struct {
+	delegate       Blobstore
+	metricsService bitsgo.MetricsService
+	resourceType   string
+}
+
+func ForBlobstoreWithMetricsEmitter(delegate Blobstore, metricsService bitsgo.MetricsService, resourceType string) *MetricsEmittingBlobstoreDecorator {
+	return &MetricsEmittingBlobstoreDecorator{delegate, metricsService, resourceType}
+}
+
+func (decorator *MetricsEmittingBlobstoreDecorator) Exists(path string) (bool, error) {
+	return decorator.delegate.Exists(path)
+}
+
+func (decorator *MetricsEmittingBlobstoreDecorator) HeadOrRedirectAsGet(path string) (redirectLocation string, err error) {
+	return decorator.delegate.HeadOrRedirectAsGet(path)
+}
+
+func (decorator *MetricsEmittingBlobstoreDecorator) Get(path string) (body io.ReadCloser, err error) {
+	return decorator.delegate.Get(path)
+}
+
+func (decorator *MetricsEmittingBlobstoreDecorator) GetOrRedirect(path string) (body io.ReadCloser, redirectLocation string, err error) {
+	return decorator.delegate.GetOrRedirect(path)
+}
+
+func (decorator *MetricsEmittingBlobstoreDecorator) Put(path string, src io.ReadSeeker) error {
+	startTime := time.Now()
+	e := decorator.delegate.Put(path, src)
+	decorator.metricsService.SendTimingMetric(decorator.resourceType+"-cp_to_blobstore-time", time.Since(startTime))
+	return e
+}
+
+func (decorator *MetricsEmittingBlobstoreDecorator) Copy(src, dest string) error {
+	return decorator.delegate.Copy(src, dest)
+}
+
+func (decorator *MetricsEmittingBlobstoreDecorator) Delete(path string) error {
+	return decorator.delegate.Delete(path)
+}
+
+func (decorator *MetricsEmittingBlobstoreDecorator) DeleteDir(prefix string) error {
+	return decorator.delegate.DeleteDir(prefix)
+}
