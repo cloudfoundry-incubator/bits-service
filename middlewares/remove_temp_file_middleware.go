72947907db4b89c150fc6357d471b0a01b19d6f0
Peter Goetz <peter.gtz@gmail.com>
2018-05-14 12:52:12 +0200

    Work around a bug in golang's multipart package
    This has filled our disk with temp files.

---
+++ middlewares/remove_temp_file_middleware.go
@@ -0,0 +1,20 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/petergtz/bitsgo/logger"
+)
+
+// This is actually implemented in golang's formdata.go#readForm. But it has bug. Working around it here.
+// TODO: reference Github issue here when it is available.
+type RemoveTempFilesMiddleware struct{}
+
+func (m *RemoveTempFilesMiddleware) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request, next http.HandlerFunc) {
+	next(responseWriter, request)
+
+	e := request.MultipartForm.RemoveAll()
+	if e != nil {
+		logger.From(request).Errorw("Could not delete multipart temporary files", "error", e)
+	}
+}
