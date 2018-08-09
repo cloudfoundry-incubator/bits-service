3756b2145a5ff80392d4066e8c49bb348e817fd1
Peter Goetz <peter.gtz@gmail.com>
2018-08-09 21:04:13 +0200

    Test that PanicMiddleware writes correct response

--- middlewares/panic_middleware_test.go
+++ middlewares/panic_middleware_test.go
@@ -1,12 +1,13 @@
 package middlewares_test
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 
+	"github.com/cloudfoundry-incubator/bits-service/middlewares"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/cloudfoundry-incubator/bits-service/middlewares"
 	"github.com/pkg/errors"
 )
 
@@ -14,15 +15,25 @@ var _ = Describe("PanicMiddleWare", func() {
 	Context("Handler panics", func() {
 		It("catches a panic and responds with an Internal Server Error", func() {
 			responseWriter := httptest.NewRecorder()
+			r := httptest.NewRequest("GET", "http://example.com/some/request", nil)
+			c := r.Context()
+			c = context.WithValue(c, "request-id", "123456")
+			c = context.WithValue(c, "vcap-request-id", "123456-7890-1234")
 
 			(&middlewares.PanicMiddleware{}).ServeHTTP(
 				responseWriter,
-				httptest.NewRequest("GET", "http://example.com/some/request", nil),
+				r.WithContext(c),
 				func(http.ResponseWriter, *http.Request) {
 					panic(errors.New("Some unexpected error"))
 				})
 
 			Expect(responseWriter.Code).To(Equal(http.StatusInternalServerError))
+			Expect(responseWriter.Body.String()).To(MatchJSON(`{
+				"service": "Bits-Service",
+				"error": "Internal Server Error",
+				"request-id": "123456",
+				"vcap-request-id":"123456-7890-1234"
+			}`))
 		})
 	})
 
