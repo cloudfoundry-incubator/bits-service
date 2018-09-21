817337c2337d2eec4c42f96b3d0500673c5cc845
Peter Goetz <pego@de.ibm.com>
2018-09-21 14:47:45 +0200

    Small fixes and refactorings in Alibaba blobstore
    [#157928837]

--- blobstores/alibaba/alibaba_blobstore.go
+++ blobstores/alibaba/alibaba_blobstore.go
@@ -1,6 +1,7 @@
 package alibaba
 
 import (
+	"fmt"
 	"io"
 	"strings"
 	"time"
@@ -8,15 +9,15 @@ import (
 	"github.com/cloudfoundry-incubator/bits-service"
 	"github.com/pkg/errors"
 
-	oss "github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"github.com/cloudfoundry-incubator/bits-service/blobstores/validate"
 	"github.com/cloudfoundry-incubator/bits-service/config"
 	"github.com/cloudfoundry-incubator/bits-service/logger"
 )
 
 type Blobstore struct {
-	BucketName string
-	Client     *oss.Client
+	Client *oss.Client
+	bucket *oss.Bucket
 }
 
 func NewBlobstore(config config.AlibabaBlobstoreConfig) *Blobstore {
@@ -29,80 +30,77 @@ func NewBlobstore(config config.AlibabaBlobstoreConfig) *Blobstore {
 	if err != nil {
 		panic(err)
 	}
+	bucket, e := client.Bucket(config.BucketName)
+	if e != nil {
+		panic(fmt.Errorf("could not get bucket"))
+	}
 	return &Blobstore{
-		BucketName: config.BucketName,
-		Client:     client,
+		bucket: bucket,
+		Client: client,
 	}
 }
 
 func (blobstore *Blobstore) Copy(src string, dest string) error {
-	var copyErr error
-	logger.Log.Debugw("Copy in Alibaba", "bucket", blobstore.BucketName, "src", src, "dest", dest)
-	bucket := blobstore.getBucket()
-	_, copyErr = bucket.CopyObjectFrom(blobstore.BucketName, src, dest)
-	if copyErr != nil {
-		return errors.Wrapf(copyErr, "Error while trying to copy src %v to dest %v in bucket %v", src, dest, blobstore.BucketName)
+	logger.Log.Debugw("Copy in Alibaba", "bucket", blobstore.bucket.BucketName, "src", src, "dest", dest)
+	_, e := blobstore.bucket.CopyObject(src, dest)
+	if e != nil {
+		return errors.Wrapf(e, "Error while trying to copy src %v to dest %v in bucket %v", src, dest, blobstore.bucket.BucketName)
 	}
-	return copyErr
+	return nil
 }
 
-func (blobstore *Blobstore) Delete(resource string) error {
-	bucket := blobstore.getBucket()
-	return bucket.DeleteObject(resource)
-
+func (blobstore *Blobstore) Delete(path string) error {
+	e := blobstore.bucket.DeleteObject(path)
+	if e != nil {
+		return errors.Wrapf(e, "Path %v", path)
+	}
+	return nil
 }
 
 func (blobstore *Blobstore) DeleteDir(prefix string) error {
-	var deletionError error
 	deletionErrs := []error{}
-	bucket := blobstore.getBucket()
-	prefixFilter := oss.Prefix(prefix)
 	marker := oss.Marker("")
-	portion := 20
 
 	for {
-		objList, err := bucket.ListObjects(oss.MaxKeys(portion), marker, prefixFilter)
-		if err != nil {
-			return errors.Wrapf(err, "Prefix %v", prefix)
+		objList, e := blobstore.bucket.ListObjects(oss.MaxKeys(20), marker, oss.Prefix(prefix))
+		if e != nil {
+			return errors.Wrapf(e, "Prefix %v", prefix)
 		}
-		deletionErrs = blobstore.deleteObjects(objList)
+		deletionErrs = append(deletionErrs, blobstore.deleteObjects(objList)...)
 		marker = oss.Marker(objList.NextMarker)
 		if !objList.IsTruncated {
 			break
 		}
 	}
 
-	if len(deletionErrs) < 0 {
-		deletionError = errors.Errorf("Prefix %v, errors from deleting: %v", prefix, deletionErrs)
+	if len(deletionErrs) > 0 {
+		return errors.Errorf("Prefix %v, errors from deleting: %v", prefix, deletionErrs)
 	}
-	return deletionError
+	return nil
 }
 
 func (blobstore *Blobstore) deleteObjects(objListResult oss.ListObjectsResult) []error {
-	bucket := blobstore.getBucket()
 	deletionErrs := []error{}
 	for _, obj := range objListResult.Objects {
-		err := bucket.DeleteObject(obj.Key)
-		if err != nil {
-			deletionErrs = append(deletionErrs, err)
+		e := blobstore.bucket.DeleteObject(obj.Key)
+		if e != nil {
+			deletionErrs = append(deletionErrs, e)
 		}
 	}
 	return deletionErrs
 }
 
-func (blobstore *Blobstore) Exists(resource string) (bool, error) {
-	bucket := blobstore.getBucket()
-	return bucket.IsObjectExist(resource)
+func (blobstore *Blobstore) Exists(path string) (bool, error) {
+	return blobstore.bucket.IsObjectExist(path)
 }
 
 func (blobstore *Blobstore) Get(path string) (io.ReadCloser, error) {
-	logger.Log.Debugw("GET", "bucket", blobstore.BucketName, "path", path)
-	exists, _ := blobstore.Client.IsBucketExist(blobstore.BucketName)
+	logger.Log.Debugw("GET", "bucket", blobstore.bucket.BucketName, "path", path)
+	exists, _ := blobstore.Client.IsBucketExist(blobstore.bucket.BucketName)
 	if !exists {
-		return nil, errors.Errorf("Bucket not found: '%v'", blobstore.BucketName)
+		return nil, errors.Errorf("Bucket not found: '%v'", blobstore.bucket.BucketName)
 	}
-	bucket := blobstore.getBucket()
-	obj, err := bucket.GetObject(path)
+	obj, err := blobstore.bucket.GetObject(path)
 	if err != nil {
 		return nil, bitsgo.NewNotFoundErrorWithMessage("Could not find object: " + path)
 	}
@@ -114,31 +112,33 @@ func (blobstore *Blobstore) GetOrRedirect(path string) (io.ReadCloser, string, e
 	return nil, signedURL, err
 }
 
-func (blobstore *Blobstore) HeadOrRedirectAsGet(resource string) (string, error) {
-	bucket := blobstore.getBucket()
-	return bucket.SignURL(resource, oss.HTTPGet, getValidityPeriod(time.Now().Add(1*time.Hour)))
+func (blobstore *Blobstore) HeadOrRedirectAsGet(path string) (string, error) {
+	return blobstore.bucket.SignURL(path, oss.HTTPGet, getValidityPeriod(time.Now().Add(1*time.Hour)))
 }
 
 func (blobstore *Blobstore) Put(path string, rs io.ReadSeeker) error {
-	logger.Log.Debugw("Put", "bucket", blobstore.BucketName, "path", path)
-	exists, _ := blobstore.Client.IsBucketExist(blobstore.BucketName)
+	logger.Log.Debugw("Put", "bucket", blobstore.bucket.BucketName, "path", path)
+	exists, _ := blobstore.Client.IsBucketExist(blobstore.bucket.BucketName)
 	if !exists {
-		return errors.Errorf("Bucket not found: '%v'", blobstore.BucketName)
+		return errors.Errorf("Bucket not found: '%v'", blobstore.bucket.BucketName)
 	}
-	bucket := blobstore.getBucket()
-	return bucket.PutObject(path, rs)
+	return blobstore.bucket.PutObject(path, rs)
 }
 
-func (blobstore *Blobstore) Sign(resource string, method string, timestamp time.Time) string {
-	var signedURL string
-	bucket := blobstore.getBucket()
+func (blobstore *Blobstore) Sign(path string, method string, timestamp time.Time) string {
+	var ossMethod oss.HTTPMethod
 	switch strings.ToLower(method) {
 	case "put":
-		signedURL, _ = bucket.SignURL(resource, oss.HTTPPut, getValidityPeriod(timestamp))
+		ossMethod = oss.HTTPPut
 	case "get":
-		signedURL, _ = bucket.SignURL(resource, oss.HTTPGet, getValidityPeriod(timestamp))
+		ossMethod = oss.HTTPGet
+
 	default:
 		panic("Supported methods are 'put' and 'get'.Got'" + method + "'")
+	}
+	signedURL, e := blobstore.bucket.SignURL(path, ossMethod, getValidityPeriod(timestamp))
+	if e != nil {
+		panic(e)
 	}
 	return signedURL
 }
@@ -146,12 +146,4 @@ func (blobstore *Blobstore) Sign(resource string, method string, timestamp time.
 func getValidityPeriod(timestamp time.Time) int64 {
 	duration := timestamp.Sub(time.Now()).Seconds()
 	return int64(duration)
-}
-
-func (blobstore *Blobstore) getBucket() *oss.Bucket {
-	bucket, err := blobstore.Client.Bucket(blobstore.BucketName)
-	if err != nil {
-		panic("Can't connect to bucket " + blobstore.BucketName)
-	}
-	return bucket
 }
