4c32f1a938f6e941b6739b454cf5eccd472ffee5
Kiran Jain <kiran.jain2@ibm.com>
2019-04-16 11:02:22 +0200

    Add integartion test for webdav blobstore path
    [#163383056]
    
    Signed-off-by: Tobias Zipfel <tobias.zipfel@de.ibm.com>

--- blobstores/webdav/webdav_blobstore_test.go
+++ blobstores/webdav/webdav_blobstore_test.go
@@ -1,24 +1,50 @@
 package webdav_test
 
 import (
+	//"fmt"
+	"net/http"
+	"net/http/httptest"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
 	. "github.com/cloudfoundry-incubator/bits-service/blobstores/webdav"
 )
 
-var _ = Describe("WebdavBlobstore", func() {
-
+var _ = Describe("webdav DeleteDirectory", func() {
 	It("appends a slash if there is none", func() {
 		prefix := AppendsSuffixIfNeeded("/some/path")
 		Expect(prefix).ToNot(HaveSuffix("//"))
 		Expect(prefix).To(Equal("/some/path/"))
-
 	})
 	It("does not append a slash if there already is a slash at the end", func() {
 		prefix := AppendsSuffixIfNeeded("/some/path/")
 		Expect(prefix).ToNot(HaveSuffix("//"))
 		Expect(prefix).To(Equal("/some/path/"))
 	})
+	Context("webdav integration test", func() {
+		var webdavBlobstore *Blobstore
+		BeforeEach(func() {
+			testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+				Expect(req.URL).ToNot(HaveSuffix("//"))
+				Expect(req.URL).To(HaveSuffix("/"))
+			}))
 
+			defer func() { testServer.Close() }()
+
+			webdavBlobstore = &Blobstore{
+				WebdavPrivateEndpoint: testServer.URL,
+				WebdavPublicEndpoint:  testServer.URL,
+				HttpClient:            &http.Client{},
+				WebdavUsername:        "foo",
+				WebdavPassword:        "bar",
+			}
+		})
+		It("appends slash to url if needed", func() {
+			webdavBlobstore.DeleteDir("/path/without/slash/suffix")
+		})
+		It("does not append a slach when there is already a slas at the end", func() {
+			webdavBlobstore.DeleteDir("path/with/single/slash/suffix/")
+		})
+	})
 })
